a9ed6f6bd51b55e1b970bd3d4f8212e47bedeb40
piyawoot <piyawoot@5b4e65d5-3729-0410-870f-a38d3dbf97fc>
2010-07-14 23:05:29 +0000

    Added support for classpath scripts in maven-plugin

--- trunk/maven-plugin/src/main/java/org/twdata/enchanter/EnchanterMojo.java
+++ trunk/maven-plugin/src/main/java/org/twdata/enchanter/EnchanterMojo.java
@@ -23,6 +23,7 @@ import java.util.Properties;
 public class EnchanterMojo extends AbstractMojo
 {
     private static final String PYTHON_EXTENSION = ".py";
+    private static final String CLASSPATH_RESOURCE_PREFIX = "classpath:";
 
    /**
     * Script file to execute.
@@ -31,7 +32,7 @@ public class EnchanterMojo extends AbstractMojo
     * @parameter
     * @required
     */
-    private String scriptFile;
+    protected String scriptFile;
 
    /**
     * The config file which will be used to resolve variables in scriptFile.
@@ -39,7 +40,7 @@ public class EnchanterMojo extends AbstractMojo
     *
     * @parameter
     */
-    private String configFile;
+    protected String configFile;
 
    /**
     * The Maven Project Object
@@ -59,66 +60,159 @@ public class EnchanterMojo extends AbstractMojo
     */
     private MavenSession session;
 
+    public MavenProject getProject() {
+        return project;
+    }
+
+    public MavenSession getSession() {
+        return session;
+    }
+
+    /**
+     * Extension point for hardcoding scriptFile for specific use.
+     */
+    protected String getScriptFile()
+    {
+        return scriptFile;
+    }
+
+    /**
+     * Extension point for hardcoding configFile for specific use.
+     */
+    protected String getConfigFile()
+    {
+        return configFile;
+    }
+
+    /**
+     * Read all the content from the reader and return as a single string.
+     */
+    private String slurp(final BufferedReader reader) throws MojoExecutionException {
+
+        // this will store the content
+        final StringBuilder content = new StringBuilder();
+        String line;
+
+        try
+        {
+            while((line=reader.readLine()) != null)
+            {
+                content.append(line);
+                content.append(System.getProperty("line.separator"));
+            }
+        }
+        catch (IOException e)
+        {
+            throw new MojoExecutionException("Error reading file", e);
+        }
+        finally
+        {
+            try
+            {
+                reader.close();
+            }
+            catch (IOException e)
+            {
+                throw new MojoExecutionException("Error closing file", e);
+            }
+        }
+
+        return content.toString();
+    }
+
+    /**
+     * Get reader from the given path which could be 1)resource in classpath 2) actual file on file system.
+     * The caller is responsible for closing the returned reader.
+     */
+    private BufferedReader getReader(String path) throws MojoExecutionException {
+
+        // if the path is a classpath resource
+        if (path.startsWith(CLASSPATH_RESOURCE_PREFIX))
+        {
+            final String resourcePath = path.replaceFirst(CLASSPATH_RESOURCE_PREFIX, "");
+            getLog().info("reading resource from classpath:" + resourcePath);
+            return new BufferedReader(new InputStreamReader(getClass().getResourceAsStream(resourcePath)));
+        }
+
+        // the path is for an actual file on filesystem.
+        else
+        {
+            // then we read from the actual file
+            try
+            {
+                getLog().info("reading resource from filesystem:" + path);
+                return new BufferedReader(new FileReader(path));
+            }
+            catch (FileNotFoundException e)
+            {
+                // bad things can happen such as the file doesn't exist or no permission to read.
+                throw new MojoExecutionException("Error opening file", e);
+            }
+        }
+    }
+
     /**
      * Execute the Mojo. This runs the supplied scriptFile.
      */
     public void execute() throws MojoExecutionException, MojoFailureException
     {
-        // the scriptFile parameter must be supplied.
-        if (scriptFile == null || scriptFile.length() == 0)
+        getLog().info("====================Executing====================");
+
+        // the path is null or empty.
+        if (getScriptFile() == null || getScriptFile().length() == 0)
         {
-            throw new MojoExecutionException("scriptFile parameter is needed for executing the EnchanterMojo");
+            throw new MojoExecutionException("missing the required scriptFile parameter");
         }
 
-        // get the absolute path.
-        final String absoluteScriptPath = resolveAbsolutePath(scriptFile);
-
-        // ensure that the script file exists.
-        final File scriptFp = new File(absoluteScriptPath);
-        if (!scriptFp.exists())
-        {
-            throw new MojoExecutionException(String.format("the specified scriptFile %s doesn't exist.", absoluteScriptPath));
-        }
-
-        // ensure that we have the right permission to read it.
-        if (!scriptFp.canRead()) {
-            throw new MojoExecutionException(String.format("cannot read the scriptFile %s due to permissions", absoluteScriptPath));
-        }
-
-        getLog().info("====================Executing enchanter====================");
+        // read the content of the scriptFile.
+        final String scriptContent = slurp(getReader(getScriptFile()));
 
         // delegate to the right executor.
-        if (scriptFile.endsWith(PYTHON_EXTENSION))
+        if (getScriptFile().endsWith(PYTHON_EXTENSION))
         {
             // the map for keeping all variables in the config file.
-            Map<String, String> variables;
+            final Map<String, String> variables;
 
-            if (configFile == null || configFile.length() == 0)
+            if (getConfigFile() == null || getConfigFile().length() == 0)
             {
                 getLog().info("No config file supplied. No variable resolution occurred in the scriptFile");
                 variables  = Collections.emptyMap();
             }
             else
             {
-                // load config file
-                String absoluteConfigPath = resolveAbsolutePath(configFile);
-                File configFp = new File(absoluteConfigPath);
-
-                Properties props = new Properties();
+                // load the content of properties file.
+                final Properties props = new Properties();
+                Reader configReader = null;
                 try
                 {
-                    props.load(new FileInputStream(configFp));
+                    configReader = getReader(getConfigFile());
+                    // this load operation doesn't close the reader when done
+                    props.load(configReader);
                 }
                 catch (IOException e)
                 {
-                    throw new MojoExecutionException(String.format("the specified configFile %s cannot be opened.", absoluteConfigPath));
+                    throw new MojoExecutionException(String.format("Error while reading the configFile:%s", getConfigFile()), e);
+                }
+                finally
+                {
+                    if (configReader != null)
+                    {
+                        try
+                        {
+                            configReader.close();
+                        }
+                        catch (IOException e)
+                        {
+                            throw new MojoExecutionException("Error while finishing the reading of the configFile", e);
+                        }
+                    }
                 }
 
                 // load all the properties to the variables map.
                 variables = new HashMap<String, String>((Map)props);
             }
 
-            executeInPython(scriptFp.getAbsolutePath(), variables);
+            executeInPython(scriptContent, variables);
         }
         else
         {
@@ -130,41 +224,45 @@ public class EnchanterMojo extends AbstractMojo
     }
 
     /**
-     * Resolve the given scriptFile to the absolute path.
-     */
-    private String resolveAbsolutePath(String filename)
-    {
-        return filename;
-    }
-
-    /**
      * Execute the python script.
      *
-     * @param scriptLocation the script file location.
+     * @param scriptContent the script to be executed (content, not file location).
      * @param configs the user configurations.
      */
-    private void executeInPython(String scriptLocation, Map<String, String> configs)
+    private void executeInPython(String scriptContent, Map<String, String> configs)
     {
         // The script interpreter
         final PythonInterpreter interp = new PythonInterpreter();
         final StreamConnection conn = new DefaultStreamConnection();
 
+        // info for users
+        if (getProject() != null)
+        {
+            getLog().info("variable mavenProject available");
+        }
+        if (getSession() != null)
+        {
+            getLog().info("variable mavenSession available");
+        }
+
         // pass variables to the python interpreter.
         interp.set("ssh", conn);
         interp.set("conn", conn);
-        interp.set("mavenProject", project);
-        interp.set("mavenSession", session);
+        interp.set("mavenProject", getProject());
+        interp.set("mavenSession", getSession());
 
         // pass on all the user configs
         for(Map.Entry<String, String> pair:configs.entrySet())
         {
+            // print out the parameter being passed
             getLog().info("variable " + pair.getKey()
                             + "(" + pair.getValue().getClass().toString() + ") ="
                             + pair.getValue());
+            // make the parameter known by the interpreter
             interp.set(pair.getKey(), pair.getValue());
         }
 
         // execute the script
-        interp.execfile(scriptLocation);
+        interp.exec(scriptContent);
     }
 }
